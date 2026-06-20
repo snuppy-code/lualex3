c212003261a9fbf7749ff46c2a965d7ed3fa964b
snuppy <snuppy.code+git@pm.me>
2026-06-21 00:38:58 +0200

    update flake

--- flake.nix
+++ flake.nix
@@ -4,26 +4,25 @@
   description = "lualexer3 development flake";
 
   inputs = {
-    # nixpkgs.url = "github:nixos/nixpkgs?ref=26.05"; # which of these two is preferable?
     nixpkgs.url = "github:nixos/nixpkgs/nixos-26.05";
   };
 
-  outputs =
-    { self, nixpkgs }:
-    let
-      my = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages."${my}";
-    in
-    {
-      devShells."${my}".default = pkgs.mkShell {
-        buildInputs = with pkgs; [
-          cargo
-          rustc
-          rustfmt
-          clippy
-          # rust-analyzer # doesn't seem necessary?
-        ];
-        env.RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
-      };
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    my = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages."${my}";
+  in {
+    devShells."${my}".default = pkgs.mkShell {
+      buildInputs = with pkgs; [
+        cargo
+        rustc
+        rustfmt
+        clippy
+        # rust-analyzer # doesn't seem necessary?
+      ];
+      env.RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
     };
+  };
 }
